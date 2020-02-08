3b432ba1e0ad56b47be5ad6e20477b881fcf9f91
Chris Heppell <clheppell@icloud.com>
2020-02-08 16:05:22 +0000

    Add email code

--- index.php
+++ index.php
@@ -13,13 +13,64 @@ $route		= $app->route;
 
 header("content-type: application/json");
 
-$route->get('/', function() {
+$route->get('/average', function() {
   if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
     http_response_code(401);
     echo json_encode("Unauthorised");
     return;
   }
-  echo json_encode("Test page");
+  $email = new \SendGrid\Mail\Mail();
+  $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
+  $email->setSubject("Your latest average temperature reading");
+  $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
+  $email->addContent("text/plain", "The average temperature reading is " . ((float) $_GET['temp']) . "°C");
+  $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+  try {
+    $response = $sendgrid->send($email);
+    echo json_encode("Sent");
+  } catch (Exception $e) {
+    echo json_encode(['exception' => $e->getMessage()]);
+  }
+});
+
+$route->get('/max', function() {
+  if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
+    http_response_code(401);
+    echo json_encode("Unauthorised");
+    return;
+  }
+  $email = new \SendGrid\Mail\Mail();
+  $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
+  $email->setSubject("Your latest max temperature reading");
+  $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
+  $email->addContent("text/plain", "The max temperature reading is " . ((float) $_GET['temp']) . "°C");
+  $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+  try {
+    $response = $sendgrid->send($email);
+    echo json_encode("Sent");
+  } catch (Exception $e) {
+    echo json_encode(['exception' => $e->getMessage()]);
+  }
+});
+
+$route->get('/centre', function() {
+  if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
+    http_response_code(401);
+    echo json_encode("Unauthorised");
+    return;
+  }
+  $email = new \SendGrid\Mail\Mail();
+  $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
+  $email->setSubject("Your latest centre screen temperature reading");
+  $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
+  $email->addContent("text/plain", "The temperature reading at the centre of the screen is " . ((float) $_GET['temp']) . "°C");
+  $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+  try {
+    $response = $sendgrid->send($email);
+    echo json_encode("Sent");
+  } catch (Exception $e) {
+    echo json_encode(['exception' => $e->getMessage()]);
+  }
 });
 
 $route->any('/*', function() {
