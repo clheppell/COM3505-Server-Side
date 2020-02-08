e90b904284e2421ae8faf66b2cc17ed2d582bf34
Chris Heppell <clheppell@icloud.com>
2020-02-08 16:12:16 +0000

    Test

--- index.php
+++ index.php
@@ -15,65 +15,69 @@ try {
 
   header("content-type: application/json");
 
-  $route->get('/average', function() {
-    if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
-      http_response_code(401);
-      echo json_encode("Unauthorised");
-      return;
-    }
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
-    $email->setSubject("Your latest average temperature reading");
-    $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
-    $email->addContent("text/plain", "The average temperature reading is " . ((float) $_GET['temp']) . "°C");
-    $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
-    try {
-      $response = $sendgrid->send($email);
-      echo json_encode("Sent");
-    } catch (Exception $e) {
-      echo json_encode(['exception' => $e->getMessage()]);
-    }
+  $route->get('/', function() {
+    echo json_encode(['status' => true]);
   });
 
-  $route->get('/max', function() {
-    if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
-      http_response_code(401);
-      echo json_encode("Unauthorised");
-      return;
-    }
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
-    $email->setSubject("Your latest max temperature reading");
-    $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
-    $email->addContent("text/plain", "The max temperature reading is " . ((float) $_GET['temp']) . "°C");
-    $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
-    try {
-      $response = $sendgrid->send($email);
-      echo json_encode("Sent");
-    } catch (Exception $e) {
-      echo json_encode(['exception' => $e->getMessage()]);
-    }
-  });
+  // $route->get('/average', function() {
+  //   if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
+  //     http_response_code(401);
+  //     echo json_encode("Unauthorised");
+  //     return;
+  //   }
+  //   $email = new \SendGrid\Mail\Mail();
+  //   $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
+  //   $email->setSubject("Your latest average temperature reading");
+  //   $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
+  //   $email->addContent("text/plain", "The average temperature reading is " . ((float) $_GET['temp']) . "°C");
+  //   $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+  //   try {
+  //     $response = $sendgrid->send($email);
+  //     echo json_encode("Sent");
+  //   } catch (Exception $e) {
+  //     echo json_encode(['exception' => $e->getMessage()]);
+  //   }
+  // });
 
-  $route->get('/centre', function() {
-    if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
-      http_response_code(401);
-      echo json_encode("Unauthorised");
-      return;
-    }
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
-    $email->setSubject("Your latest centre screen temperature reading");
-    $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
-    $email->addContent("text/plain", "The temperature reading at the centre of the screen is " . ((float) $_GET['temp']) . "°C");
-    $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
-    try {
-      $response = $sendgrid->send($email);
-      echo json_encode("Sent");
-    } catch (Exception $e) {
-      echo json_encode(['exception' => $e->getMessage()]);
-    }
-  });
+  // $route->get('/max', function() {
+  //   if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
+  //     http_response_code(401);
+  //     echo json_encode("Unauthorised");
+  //     return;
+  //   }
+  //   $email = new \SendGrid\Mail\Mail();
+  //   $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
+  //   $email->setSubject("Your latest max temperature reading");
+  //   $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
+  //   $email->addContent("text/plain", "The max temperature reading is " . ((float) $_GET['temp']) . "°C");
+  //   $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+  //   try {
+  //     $response = $sendgrid->send($email);
+  //     echo json_encode("Sent");
+  //   } catch (Exception $e) {
+  //     echo json_encode(['exception' => $e->getMessage()]);
+  //   }
+  // });
+
+  // $route->get('/centre', function() {
+  //   if ($_GET['secret'] != $_ENV['SECRET_KEY']) {
+  //     http_response_code(401);
+  //     echo json_encode("Unauthorised");
+  //     return;
+  //   }
+  //   $email = new \SendGrid\Mail\Mail();
+  //   $email->setFrom($_ENV['EMAIL_FROM'], "COM3505 SERVER CLIENT");
+  //   $email->setSubject("Your latest centre screen temperature reading");
+  //   $email->addTo($_ENV['EMAIL_TO'], "unPhone User");
+  //   $email->addContent("text/plain", "The temperature reading at the centre of the screen is " . ((float) $_GET['temp']) . "°C");
+  //   $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+  //   try {
+  //     $response = $sendgrid->send($email);
+  //     echo json_encode("Sent");
+  //   } catch (Exception $e) {
+  //     echo json_encode(['exception' => $e->getMessage()]);
+  //   }
+  // });
 
   $route->any('/*', function() {
     http_response_code(404);
